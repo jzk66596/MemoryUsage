b7a83911d69c691f9e2e3815af336cf5fef6a04a
jzk66596 <184367594@qq.com>
2015-12-16 12:37:56 -0600

    memory.cpp

---
+++ memory.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/time.h>
+#include <vector>
+using namespace std;
+
+vector<int> vec;
+
+int main() {
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+  long starttime = start.tv_sec * 1000000 + start.tv_usec;
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 130000000; ++j) {
+      vec.push_back(j);
+    }
+  }
+  gettimeofday(&end, NULL);
+  long endtime = end.tv_sec * 1000000 + end.tv_usec;
+  float runtime = (end.tv_sec + (end.tv_usec * 1.0 / 1000000)) - (start.tv_sec + (start.tv_usec * 1.0 / 1000000));
+
+  cout << starttime << " " << endtime << " " << runtime << endl;
+  while (1) {
+    vec[0];
+    usleep(1000);
+  }
+}
